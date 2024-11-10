1468710264b7d3208379a7db00fb1d5c1b360e2f
Silmar <silmarfa@yahoo.com.br>
2024-11-10 08:27:19 -0300

    feat: projeto inicial NovoFdigital

---
+++ projeto/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('sistema.urls')),
+    path('solicitacoes/', include('solicitacoes.urls')),
+    path('distribuicao/', include('distribuicao.urls', namespace='distribuicao')),
+
+
+]
